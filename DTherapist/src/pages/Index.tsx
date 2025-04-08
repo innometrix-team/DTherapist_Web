bb86a5505c6ba0817387a55f27ab40071d067c25
AsukFavour <favourasuk@gmail.com>
2025-04-08 19:40:22 +0100

    add about and service card on the home page

--- DTherapist/src/pages/Index.tsx
+++ DTherapist/src/pages/Index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
+import ServiceList from '../components/ServiceCards/ServiceList'
+import AboutSection from '../components/AboutSection'
 
 function Index() {
   return (
     <>
     <Navbar/>
     <HeroSection/>
+    <ServiceList/>
+    <AboutSection/>
     </>
   )
 }
---
+++ DTherapist/src/types/index.ts
@@ -0,0 +1,14 @@
+
+export interface Service {
+    title: string;
+    imageUrl: string;
+    description: string;
+  }
+  
+  
+  export interface Step {
+    icon: React.ReactNode;
+    title: string;
+    desc: string;
+  }
+  
