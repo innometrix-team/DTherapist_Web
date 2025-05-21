e8141cdda7551693e104a9ef496596a28baaf1c6
David Johnson <48daveed@gmail.com>
2025-05-21 13:48:12 +0100

    feat: implement dashboard overview and stats

--- apps/dashboard/src/assets/icons/index.ts
+++ apps/dashboard/src/assets/icons/index.ts
@@ -7,3 +7,7 @@ export { default as ScheduleIcon }     from "./Schedule.svg?react";
 export { default as SettingsIcon }     from "./Settings.svg?react";
 export { default as LogoutIcon }       from "./Logout.svg?react";
 export { default as MenuIcon }         from "./MenuIcon.svg?react";
+export {default as TopUpIcon} from "./topup.svg?react";
+export {default as WithdrawIcon} from "./withdraw.svg?react";
+export {default as TimerIcon} from "./timer.svg?react";
+export {default as UptrendIcon} from "./uptrend.svg?react";
--- apps/dashboard/src/components/layout/Layout.tsx
+++ apps/dashboard/src/components/layout/Layout.tsx
@@ -33,7 +33,7 @@ const Layout: React.FC = () => {
           </div>
         </header>
 
-        <main className="flex-1 overflow-auto p-6">
+        <main className="flex-1 overflow-y-auto p-6 bg-[#f7f7f8]">
           <Outlet />
         </main>
       </div>
